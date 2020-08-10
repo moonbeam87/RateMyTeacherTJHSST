4f9f0c04287a32a91d5fbcf9000f9c322565fef4
moonbeam87 <2021dpatel@gmail.com>
2020-08-10 11:36:55 -0400

    got forms working partially?

---
+++ model.py
@@ -0,0 +1,11 @@
+from wtforms import SubmitField, BooleanField, StringField, PasswordField, validators
+from flask_wtf import Form
+
+class RegForm(Form):
+    name_first = StringField('Department', [validators.DataRequired()])
+    name_last = StringField('Teacher Last Name', [validators.DataRequired()])
+    rating = StringField('Enter Rating from 1 to 5', [
+        validators.DataRequired(),
+    ])
+    description = StringField('Elaborate On your rating... if you want')
+    submit = SubmitField('Submit')
--- server.py
+++ server.py
@@ -5,7 +5,10 @@ from wtforms import Form, TextField, TextAreaField, validators, StringField, Sub
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+import os
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
 
 class ReusableForm(Form):
   name = TextField('Name:', validators=[validators.required()])
