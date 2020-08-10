44f99ac8c35866a7f639aceec642362bcad4a99a
moonbeam87 <2021dpatel@gmail.com>
2020-08-10 11:51:05 -0400

    fixed forms :)

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 from flask import Flask, request, render_template
 from model import RegForm
 from flask_bootstrap import Bootstrap
+from write_to_table import write
 
 app = Flask(__name__)
 app.config.from_mapping(
@@ -13,7 +14,11 @@ Bootstrap(app)
 def registration():
     form = RegForm(request.form)
     if request.method == 'POST' and form.validate_on_submit():
-        return 'We confirm your registration!'
+        department = form.name_first.data
+        last_name = form.name_last.data
+        rating = form.rating.data
+        description = form.description.data
+        return write(department, last_name, rating, description)
     return render_template('registration_custom.html', form=form)
 
 if __name__ == '__main__':
