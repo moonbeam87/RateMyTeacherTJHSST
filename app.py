8e88929ea752da5dc5765bf3d84955a3bf9f2e31
moonbeam87 <2021dpatel@gmail.com>
2020-08-11 09:31:37 -0400

    fixed visibility issue

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ from flask_bootstrap import Bootstrap
 from write_to_table import write
 from return_items import get_average_rating, get_ratings
 
-app = Flask(__name__)
 
 
 nav = Nav()
+
+
+nav.register_element('top', Navbar(
+    View('Submit Rating', 'registration'),
+    View('Query by Teacher', 'registration'),
+))
+app = Flask(__name__)
 app.config.from_mapping(
     SECRET_KEY=b'\xd6\x04\xbdj\xfe\xed$c\x1e@\xad\x0f\x13,@G')
 Bootstrap(app)
-
-nav.register_element('top', Navbar(
-    View('Submit Rating', 'registration_custom'),
-    View('Query by Teacher', 'read'),
-))
-
 nav.init_app(app)
 @app.route('/read', methods=['GET', 'POST'])
 def dropdown():
